e9b888141e8958e3791c5c62d06fa99675230ce3
Adrien Lacroix <oni.dev.box@gmail.com>
2015-11-23 19:27:45 +0100

    Add Comparable interface to Hand

--- src/Entities/Hand.java
+++ src/Entities/Hand.java
@@ -2,52 +2,57 @@ package entities;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import constants.Type;
 import constants.Value;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
 
-		ArrayList<Card> cards;
-		
-		public Hand(ArrayList<Card> _cards) throws Exception {
-			if(_cards.size() >= 5 && _cards.size() <= 7) {
-				cards = new ArrayList<Card>();
-				
-				for(Card c : _cards)
-					cards.add(c);
-				
-				Collections.sort(cards);
-				this.evaluate();
-			} else {
-				System.out.println(_cards.size());
-				throw new Exception("Nombre de cartes invalide pour créer une main");
-			}
+	ArrayList<Card> cards;
+
+	public Hand(ArrayList<Card> _cards) throws Exception {
+		if(_cards.size() >= 5 && _cards.size() <= 7) {
+			cards = new ArrayList<Card>();
+
+			for(Card c : _cards)
+				cards.add(c);
+
+			Collections.sort(cards);
+			this.evaluate();
+		} else {
+			System.out.println(_cards.size());
+			throw new Exception("Nombre de cartes invalide pour créer une main");
 		}
-		
-		public void evaluate() {
-			Integer[] values = new Integer[13];
-			for(int i = 0; i < 13; i++)
-				values[i] = 0;
-			
-			Integer[] types = new Integer[4];
-			for(int i = 0; i < 4; i++)
-				types[i] = 0;
-				
-			for(Card c : cards) {
-				values[c.getValue().ordinal()]++;
-				types[c.getType().ordinal()]++;
-			}
-			
-			//test affichage
-			System.out.println("values");
-			for(Value v : Value.values()) {
-				System.out.println("\t"+v.toString()+" - "+values[v.ordinal()]);
-			}
-			System.out.println("types");
-			for(Type t : Type.values()) {
-				System.out.println("\t"+t.toString()+" - "+types[t.ordinal()]);
-			}
+	}
+
+	public void evaluate() {
+		Integer[] values = new Integer[13];
+		for(int i = 0; i < 13; i++)
+			values[i] = 0;
+
+		Integer[] types = new Integer[4];
+		for(int i = 0; i < 4; i++)
+			types[i] = 0;
+
+		for(Card c : cards) {
+			values[c.getValue().ordinal()]++;
+			types[c.getType().ordinal()]++;
 		}
-		
+
+		//test affichage
+		System.out.println("values");
+		for(Value v : Value.values()) {
+			System.out.println("\t"+v.toString()+" - "+values[v.ordinal()]);
+		}
+		System.out.println("types");
+		for(Type t : Type.values()) {
+			System.out.println("\t"+t.toString()+" - "+types[t.ordinal()]);
+		}
+	}
+
+	@Override
+	public int compareTo(Hand o) {
+		return 0;
+	}
 }
